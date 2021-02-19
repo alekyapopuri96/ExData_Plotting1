60c9fbd248af1e9ccd72dc2b4d5cf51973ba7252
Alekya Popuri <alekyapopuri96@gmail.com>
2021-02-19 14:10:21 -0500

    updated with comments

--- plot2.R
+++ plot2.R
@@ -6,12 +6,16 @@ data<-na.omit(data)
 
 ## subset the values betwees 01/02/2007 to 02/02/2007
 subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+## converting to date format
 subset$Date <- as.Date(subset$Date, format="%d/%m/%Y")
 head(subset$Date)
-
+## taking time and date 
 time<- paste(as.Date(subset$Date), subset$Time)
 time <- as.POSIXct(time)
 time
+## converting char to numeric for global active power
 Global_active_power <- as.numeric(subset$Global_active_power)
+
+## plotting time and global active power
 plot(time, Global_active_power, type='l',ylab="Global Active Power(Kilowatts)",xlab="")
 png("plot2.png")
